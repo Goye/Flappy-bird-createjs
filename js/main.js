91a1b102b89131c947e95394edc0f096ca92a62d
Carlos Goyeneche <karlos.goye@gmail.com>
2016-04-07 09:58:39 -0500

    character added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var loadingInterval = 0;
+var jump;
 
 function init() {
   canvas = document.getElementById("canvasGame");
@@ -14,7 +15,8 @@ function init() {
 
   var manifest = [  
       {id:"floor", src:"img/background1.png"},
-      {id:"clouds", src:"img/background2.png"}
+      {id:"clouds", src:"img/background2.png"},
+      {id:"sonic", src:"img/sonic.png"}
   ];
 
   preload = new createjs.LoadQueue();  
@@ -46,6 +48,23 @@ function handleClick() {
 }
 
 function restart() {  
+  var sonicImage = preload.getResult("sonic");
+  var dataSonic = new createjs.SpriteSheet({
+    "images": [sonicImage],
+    "frames": {"regX": 0, "height": 64, "count": 9, "regY": 0, "width": 64},
+    "animations": {
+      "up": [0,2,"up"],
+      "straight": [3, 5, "straight"],
+      "down": [6, 8, "down"]
+    }
+  });
+
+  sonic = new createjs.Sprite(dataSonic, "straight");  
+  sonic.framerate = 5;  
+  sonic.x = 50;  
+  sonic.y = 50;
+  jump = 0;
+
   scene.removeAllChildren();
   floorImage = preload.getResult("floor");
   floor = new createjs.Bitmap(floorImage);       
@@ -61,8 +80,9 @@ function restart() {
   clouds2 = new createjs.Bitmap(cloudsImage);  
   clouds2.x = 640;  
   clouds2.y = 0;
-  scene.addChild(clouds, clouds2, floor, floor2);
+  scene.addChild(clouds, clouds2, floor, floor2, sonic);
   scene.update();
+  canvas.onclick = doJump;
 
   if (!createjs.Ticker.hasEventListener("tick")) {  
    createjs.Ticker.addEventListener("tick", tick);
@@ -73,7 +93,7 @@ function tick(event) {
   clouds.x = clouds.x-1;
   floor.x = floor.x-2;
   clouds2.x = clouds2.x-1;  
-  floor2.x = floor2.x-2; 
+  floor2.x = floor2.x-2;
   if (floor.x == -674){  
     floor.x = 674;
   }
@@ -86,7 +106,29 @@ function tick(event) {
   if (clouds2.x == -640){  
     clouds2.x = 640;
   }
+
+  //Sonic logic
+  sonic.y = sonic.y+10;  
+  sonic.y = sonic.y-jump;  
+  if (jump > 0){  
+    jump = jump-2;
+    if (jump > 10){
+      if (sonic.currentAnimation != "up") {
+        sonic.gotoAndPlay("up");
+      }
+    } else {
+      if (sonic.currentAnimation != "straight") {
+        sonic.gotoAndPlay("straight");
+      }
+    }
+  } else if (sonic.currentAnimation != "down") {
+     sonic.gotoAndPlay("down");
+  }
   scene.update(event);
+}
+
+function doJump(){
+  jump = 20; 
 }
 
 (function(){
